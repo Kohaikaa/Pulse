428b99a8753ba54f1ba56669f0eb0aba6ba6c0d4
Vladislav Kohai <cherry.designwork@gmail.com>
2023-01-30 08:51:29 +0300

    Failed mailer

--- src/js/script.js
+++ src/js/script.js
@@ -94,11 +94,12 @@ $(document).ready(function () {
             url: "mailer/smart.php",
             data: $(this).serialize()
         }).done(function () {
-            $(this).find('input').val("");
-
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
 
             $('form').trigger('reset');
         });
         return false;
-    })
+    });
 });
