9348fe45a5d64ddda50236046bf74df501635fc3
Vladislav Kohai <cherry.designwork@gmail.com>
2023-01-30 07:56:20 +0300

    Adding Mailer

--- src/js/script.js
+++ src/js/script.js
@@ -85,4 +85,20 @@ $(document).ready(function () {
 
     // Masking inputs
     $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+    // Sending data
+    $('form').submit(function (e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function () {
+            $(this).find('input').val("");
+
+
+            $('form').trigger('reset');
+        });
+        return false;
+    })
 });
